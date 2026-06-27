5d8452ce07a68fd54b4f24e03ecf0735022860dd
Patrick Feller <fellerpatae@gmail.com>
2026-06-27 15:19:56 +0200

    fix: ass flask debug env

--- app.py
+++ app.py
@@ -320,4 +320,4 @@ def history():
     return render_template('history.html', sessions=sessions)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=os.environ.get('FLASK_DEBUG', 'false').lower() == 'true')
